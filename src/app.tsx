a54fed4e0631924ff3695fb76892a8e54fb0a7ca
Frederico Estrela Gaiva <frederico.e@miro.com>
2022-05-19 16:47:25 +0200

    Trivia questions

--- src/app.tsx
+++ src/app.tsx
@@ -1,43 +1,88 @@
-import * as React from 'react';
-import ReactDOM from 'react-dom';
+import * as React from "react";
+import ReactDOM from "react-dom";
 
-async function addSticky() {
-  const stickyNote = await miro.board.createStickyNote({
-    content: 'Hello, World!',
-  });
+import { get, generate } from "./board";
 
-  await miro.board.viewport.zoomTo(stickyNote);
-}
+const questions = [10, 20, 30, 40, 50];
+
+type Category = {
+  id: string;
+  name: string;
+};
 
 function App() {
+  const [amount, setAmount] = React.useState("10");
+  const [category, setCategory] = React.useState("");
+  const [isGenerating, setIsGenerating] = React.useState(false);
+  const [categories, setCategories] = React.useState<Category[]>([]);
+
   React.useEffect(() => {
-    addSticky();
+    const loadCategories = async () => {
+      const { trivia_categories = [] } = await get("api_category.php");
+      setCategories(trivia_categories);
+    };
+
+    loadCategories();
   }, []);
 
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    setIsGenerating(true);
+    await generate({
+      amount,
+      category,
+    });
+    setIsGenerating(false);
+
+    return false;
+  };
+
+  const handleNumberQuestions = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setAmount(e.target.value);
+  };
+
+  const handleCategory = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setCategory(e.target.value);
+  };
+
   return (
-    <div className="grid wrapper">
-      <div className="cs1 ce12">
-        <img src="/src/assets/congratulations.png" alt="" />
-      </div>
-      <div className="cs1 ce12">
-        <h1>Congratulations!</h1>
-        <p>You've just created your first Miro app!</p>
-        <p>
-          To explore more and build your own app, see the Miro Developer
-          Platform documentation.
-        </p>
-      </div>
-      <div className="cs1 ce12">
-        <a
-          className="button button-primary"
-          target="_blank"
-          href="https://developers.miro.com"
+    <form onSubmit={handleSubmit}>
+      <div className="form-group">
+        <label htmlFor="number-of-questions">
+          How many questions do you want to generate?
+        </label>
+        <select
+          className="select"
+          id="number-of-questions"
+          onChange={handleNumberQuestions}
         >
-          Read the documentation
-        </a>
+          {questions.map((q) => (
+            <option key={q} value={q}>
+              {q}
+            </option>
+          ))}
+        </select>
       </div>
-    </div>
+      <div className="form-group">
+        <label htmlFor="category">Category</label>
+        <select className="select" id="category" onChange={handleCategory}>
+          {categories.map((c) => (
+            <option key={c.id} value={c.id}>
+              {c.name}
+            </option>
+          ))}
+        </select>
+      </div>
+      <button
+        className={`button button-primary ${isGenerating && "button-loading"}`}
+        type="submit"
+        disabled={isGenerating}
+      >
+        Generate
+      </button>
+    </form>
   );
 }
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
---
+++ src/board.ts
@@ -0,0 +1,93 @@
+import cardImage from "./assets/card.jpeg";
+
+export const API_URL = "https://opentdb.com";
+const CARD_IMG_URL = `${location.protocol}//${location.host}${cardImage}`;
+
+const WIDTH = 500;
+const HEIGHT = WIDTH;
+
+const defaultCardProps = {
+  style: {
+    fillColor: "#ffffff",
+    fontSize: 20,
+  },
+  width: WIDTH,
+  height: HEIGHT,
+};
+
+function shuffle(array: unknown[]) {
+  const local = [...array];
+  for (var i = local.length - 1; i > 0; i--) {
+    var j = Math.floor(Math.random() * (i + 1));
+    var temp = local[i];
+    local[i] = local[j];
+    local[j] = temp;
+  }
+
+  return local;
+}
+
+export async function get(
+  endpoint: string = API_URL,
+  params: Record<string, string> = {}
+) {
+  const url = new URL(`${API_URL}/${endpoint}`);
+  url.search = new URLSearchParams(params).toString();
+
+  return await fetch(url.toString()).then((res) => res.json());
+}
+
+async function createTriviaCard(trivia: any) {
+  const answers = shuffle([...trivia.incorrect_answers, trivia.correct_answer]);
+
+  const answerContent = answers
+    .map((a, i) => `<p><b>${++i}</b> - ${a}</p>`)
+    .join("");
+
+  const content = `
+    <p><b>${trivia.question}</b></p>
+    <br>
+    ${answerContent}
+  `;
+
+  await miro.board.createShape({
+    ...defaultCardProps,
+    content: trivia.correct_answer,
+  });
+
+  await miro.board.createShape({
+    ...defaultCardProps,
+    content: content,
+  });
+}
+
+export async function generate(
+  opts: { amount?: string; category?: string } = {}
+) {
+  const { results = [] } = await get("api.php", opts);
+
+  for (let trivia of results) {
+    await createTriviaCard(trivia);
+  }
+
+  console.log({ CARD_IMG_URL });
+
+  const cover = await miro.board.createImage({
+    url: CARD_IMG_URL,
+    width: WIDTH,
+    height: HEIGHT,
+  });
+
+  // const cover = await miro.board.createShape({
+  //   ...defaultCardProps,
+  //   content: "Trivia time",
+  //   style: {
+  //     ...defaultCardProps.style,
+  //     fillColor: "#4262ff",
+  //     fontSize: 40,
+  //   },
+  // });
+
+  await miro.board.viewport.zoomTo(cover);
+  await miro.board.ui.closePanel();
+}
